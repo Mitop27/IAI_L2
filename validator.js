53d512e4d261095fe8d12a5b927058da3a449863
Michał Topór <43950890+Mitop27@users.noreply.github.com>
2018-11-04 19:21:12 +0100

    Add files via upload

--- validator.js
+++ validator.js
@@ -1,10 +1,41 @@
-let send=document.querySelector('.send');
-let cancel=document.querySelector('.clear');
+let send=document.querySelector(".send");
+let cancel=document.querySelector(".clear");
 
-send.addEventListener('click',function(){
+send.addEventListener("click", valid_f());
 
-let name=document.getElementById('.name');
-console.log(name.value);
+
+function add_class(element, class_name){
+    element.classList.add(class_name);
+}
+function remove_class(element, class_name){
+    element.classList.remove(class_name);
+}
+
+/* //////////////////////////////////////////// */
+
+function valid_f(){
+
+    let name = document.querySelector("#name");
+    let age = document.querySelector("#age");
+    let surname = document.querySelector("#surname");
+
+        let name_v = name.value;
+        let age_v = age.value;
+        let surname_v = surname.value;
+
+    if(age_v==='' || !parseInt(age_v) || isNaN(age_v) || age_v < 1 || age_v > 125){
+        add_class(age, "invalid");
+        document.querySelector('.age_valid').innerHTML="Wrong data!";
+
+    }
+    else{
+        remove_class(age, 'invalid');
+        add_class(age, 'valid');
+        document.querySelector('.age_valid').innerHTML="Data is correct";
+    }
+}
+
+/*
 if(name.value===""){
     document.querySelector('.name_validation').innerHTML="Wrong data!";
 }
@@ -17,10 +48,9 @@ else{
 
 
 
-});
-
-cancel.addEventListener('click',function () {
 
 
+cancel.addEventListener('click',clear_f) {
+*/
 
-});
+
